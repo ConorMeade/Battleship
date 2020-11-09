d25f1f9cc5a0d2ba2cdc3f32db2f3431ba93d145
Conor <cmeade6479@gmail.com>
2020-11-09 16:12:16 -0500

    first commit, class definitions

---
+++ BattleshipPiece.cpp
@@ -0,0 +1,18 @@
+class BattleshipPiece
+{
+private:
+    /* data */
+    int pieceLength;
+    bool destroyed;
+public:
+    BattleshipPiece(/* args */);
+    ~BattleshipPiece();
+};
+
+BattleshipPiece::BattleshipPiece(/* args */)
+{
+}
+
+BattleshipPiece::~BattleshipPiece()
+{
+}
